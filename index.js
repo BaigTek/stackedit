72234ddff69d06e945d13257eb0ba2e42d54ba3e
Wasif Hasan Baig <wasifb@netacquire.com>
2018-11-07 15:07:47 -0800

    Working Websocket - run `node index.js` in the root dir.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ Object.keys(env).forEach((key) => {
 
 const http = require('http');
 const express = require('express');
+const WebSocket = require('ws');
 
 const app = express();
 
@@ -15,6 +16,22 @@ require('./server')(app, process.env.SERVE_V4);
 
 const port = parseInt(process.env.PORT || 8080, 10);
 const httpServer = http.createServer(app);
+
+const wss = new WebSocket.Server({ server: httpServer });
+wss.on('connection', (ws) => {
+
+  // connection is up, let's add a simple simple event
+  ws.on('message', (message) => {
+
+    // log the received message and send it back to the client
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  // send immediately a feedback to the incoming connection
+  ws.send('Hi there, I am a WebSocket server');
+});
+
 httpServer.listen(port, null, () => {
   console.log(`HTTP server started: http://localhost:${port}`);
 });
